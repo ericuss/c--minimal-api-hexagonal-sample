98200dfae21c831e9df89c98edba6da2b174da62
Eric Torre <etorreg@gmail.com>
2023-01-27 13:25:03 +0100

    add user module

---
+++ src/Hex.Sample.Module.User/Configure.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Hex.Sample.Module.User
+{
+
+    public static class Configure
+    {
+        public static IServiceCollection ConfigureUsers(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ src/Hex.Sample.Module.User/Infrastructure/Http/Router.cs
@@ -0,0 +1,16 @@
+﻿namespace Hex.Sample.Module.User.Infrastructure.Http;
+
+using Hex.Sample.Module.User.Application.Users;
+using Microsoft.AspNetCore.Builder;
+
+public static class Router
+{
+    public static WebApplication RegisterUserRoutes(this WebApplication app)
+    {
+        //const string route = "/api/v{version:apiVersion}/users";
+        const string route = "/api/users";
+
+        app.MapGet(route, GetAllUsersFeature.Handle);
+        return app;
+    }
+}
